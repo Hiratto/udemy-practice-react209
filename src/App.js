5a5fcc5b2df4d847f4d1611f41c2d873556ade62
Takahisa Hirano <54578410+Hiratto@users.noreply.github.com>
2022-03-07 22:13:23 +0900

    ページの仕上げ

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "Hiratto",
-  image: "https://source.unsplash.com/adK3Vu70DEQ",
-  email: "00001234@.com",
-  phone: "000-9999-8888",
-  company: {
-    name: "テスト会社"
-  },
-  website: "12340000@.com"
-};
-
 export default function App() {
   return <Router />;
 }
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,13 +1,47 @@
 import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/userCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `Hiratto${val}`,
+    image: "https://source.unsplash.com/adK3Vu70DEQ",
+    email: "00001234@.com",
+    phone: "000-9999-8888",
+    company: {
+      name: "テスト会社"
+    },
+    website: "12340000@.com"
+  };
+});
 
 export const Users = () => {
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+        ;
+      </SUserArea>
     </SContainer>
   );
 };
 
 const SContainer = styled.div`
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
 `;
