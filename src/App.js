fca292c242b7459ae2418b0edf979b3b5778ba92
Takahisa Hirano <54578410+Hiratto@users.noreply.github.com>
2022-03-07 20:29:35 +0900

    テンプレート作成（ヘッダーオンリー）

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { BrowserRouter } from "react-router-dom";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organisms/user/userCard";
+import { HeaderOnly } from "./components/templates/HeaderOnly";
 import "./styles.css";
 
 const user = {
@@ -17,14 +19,15 @@ const user = {
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <PrimaryButton>ボタン</PrimaryButton>
-      <SecondaryButton>検索</SecondaryButton>
-      <br />
-      <SearchInput />
-      <br />
-      <UserCard user={user} />
-    </div>
+    <BrowserRouter>
+      <HeaderOnly>
+        <PrimaryButton>ボタン</PrimaryButton>
+        <SecondaryButton>検索</SecondaryButton>
+        <br />
+        <SearchInput />
+        <br />
+        <UserCard user={user} />
+      </HeaderOnly>
+    </BrowserRouter>
   );
 }
