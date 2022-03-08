f2d0d9a82a7d39f0ad253a0f306ef2d88c0bdcc0
Takahisa Hirano <54578410+Hiratto@users.noreply.github.com>
2022-03-08 21:02:27 +0900

    管理者ユーザーの時だけ編集ボタンを出す 面倒なパターン編 ＞Top/User/userCurd/UserIconWithName

--- src/components/atoms/button/SecondaryButton.jsx
+++ src/components/atoms/button/SecondaryButton.jsx
@@ -3,7 +3,7 @@ import { BaseButton } from "./BaseButton";
 
 export const SecondaryButton = (props) => {
   const { children, onClick } = props;
-  return <SButton>{children}</SButton>;
+  return <SButton onClick={onClick}>{children}</SButton>;
 };
 
 const SButton = styled(BaseButton)`
